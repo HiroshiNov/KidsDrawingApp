62d625bc59ef8b78ee3f53348d5d0a471a6495c4
Hiroshi.M <hiroshi.m.nov@gmail.com>
2020-10-31 15:33:59 +0900

    Complete lesson108

--- app/src/main/java/jp/hiroshi/nov/m/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/jp/hiroshi/nov/m/kidsdrawingapp/DrawingView.kt
@@ -18,6 +18,7 @@ class DrawingView(context: Context, attrs: AttributeSet ) : View(context, attrs)
     private var mBrushSize: Float = 0.toFloat()
     private var color = Color.BLACK
     private var canvas: Canvas? = null
+    private val mPaths = ArrayList<CustomPath>()
 
     init{
         setUpDrawing()
@@ -46,6 +47,14 @@ class DrawingView(context: Context, attrs: AttributeSet ) : View(context, attrs)
     override fun onDraw(canvas: Canvas) { //lesson107
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!, 0f ,0f, mCanvasPaint)
+
+        for(path in mPaths){ //output memorized drawings
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path,mDrawPaint!!)
+        }
+
+
         if(!mDrawPath!!.isEmpty){
             mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
             mDrawPaint!!.color = mDrawPath!!.color
@@ -78,7 +87,9 @@ class DrawingView(context: Context, attrs: AttributeSet ) : View(context, attrs)
                 }
             }
             MotionEvent.ACTION_UP -> {
-                mDrawPath = CustomPath(color,mBrushSize)
+                mPaths.add(mDrawPath!!) // memorize the Path
+                mDrawPath = CustomPath(color,mBrushSize) //release the brush
+
             }
             else -> return false
         }
